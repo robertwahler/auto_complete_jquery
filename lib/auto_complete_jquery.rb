08f381691aee16dc5b3e19a7706a70393e776158
Robert Wahler <robert@gearheadforhire.com>
2009-06-26 15:13:25 -0400

    Revert "auto_complete_for accepts block, returns value|id pairs"
    This reverts commit 83c3c42e009a07b4a7ed36dd5be16dd6985206f4.

--- lib/auto_complete_jquery.rb
+++ lib/auto_complete_jquery.rb
@@ -37,18 +37,12 @@ module AutoCompleteJquery
         find_options = { 
           :conditions => [ "LOWER(#{method}) LIKE ?", '%' + params[:q].downcase + '%' ], 
           :order => "#{method} ASC",
-          :select => "#{object_constant.table_name}.#{object_constant.primary_key},#{object_constant.table_name}.#{method}",
+          :select => "#{object_constant.table_name}.#{method}",
           :limit => 10 }.merge!(options)
         
-        @items = object_constant.find(:all, find_options)
+        @items = object_constant.find(:all, find_options).collect(&method)
 
-        if block_given?
-          content = yield(@items)
-        else
-          content = @items.map{|item| "#{item.send(method)}|#{item.send(object_constant.primary_key)}"}.join("\n")
-        end
-        render :text => content
-
+        render :text => @items.join("\n")
       end
     end
   end
